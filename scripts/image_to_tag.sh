aaa4ac844656fb2ba544c33c999fcce6dc2ece29
Vlad Grigorescu <vlad@es.net>
2020-09-04 12:07:43 -0500

    Add CentOS 6 support to image_to_tag

--- scripts/image_to_tag.sh
+++ scripts/image_to_tag.sh
@@ -4,7 +4,7 @@ function convert_image_name_to_tag() {
     declare -A NAMES
     NAMES["latest"]=master
 
-    NAMES+=( ["centos:7"]=centos_7 ["centos:8"]=centos_8 )
+    NAMES+=( ["centos:6"]=centos_6 ["centos:7"]=centos_7 ["centos:8"]=centos_8 )
 
     NAMES+=( ["ubuntu:18.04"]=ubuntu_18 ["ubuntu:20.04"]=ubuntu_20 )
 
